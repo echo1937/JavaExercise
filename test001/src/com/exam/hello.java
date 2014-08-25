40dfbebb90a5cbd9d7a6e1a5cc83237e16533ee3
Eric <18657523064@139.com>
2014-08-26 00:19:51 +0800

    MyJavaExercise

---
+++ test001/src/com/exam/hello.java
@@ -0,0 +1,10 @@
+package com.exam;
+
+/**
+ * Created by Eric on 8/26/2014.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+    }
+}
