d993b7fdedc7aa1e21c02bc833f06cf47585fb6b
Eric <18657523064@139.com>
2014-08-26 15:33:07 +0800

    8/26/2014

---
+++ sourcecode/src/com/example/hello/CharsetConverter.java
@@ -0,0 +1,55 @@
+package com.example.hello;
+
+import java.io.*;
+import java.nio.*;
+import java.nio.charset.*;
+
+public class CharsetConverter {
+    public static void main(String args[]) throws IOException {
+        final int BSIZE = 1024;
+        //代码1
+        ByteBuffer bb = ByteBuffer.wrap("你好".getBytes("UTF-8"));
+        CharBuffer cb = bb.asCharBuffer();
+        System.out.println(cb);  //打印
+
+        //代码2
+        bb = ByteBuffer.wrap("你好".getBytes("UTF-16BE"));
+        cb = bb.asCharBuffer();
+        System.out.println(cb);  //打印“你好”
+
+        //代码3
+        /*
+        bb = ByteBuffer.wrap("你好".getBytes("UTF-8"));
+        Charset cs = Charset.forName("UTF-8");
+        cb = cs.decode(bb);
+        */
+
+        Charset cs;
+        cb=Charset.forName("UTF-8").decode(ByteBuffer.wrap("你好".getBytes("UTF-8")));
+        System.out.println(cb);  //打印“你好”
+
+
+
+        //代码4
+
+//        cs = Charset.forName("GBK");
+//        bb = cs.encode("你好");
+//        cb = cs.decode(bb);
+
+//        bb=Charset.forName("GBK").encode("你好");
+//        cb=Charset.forName("GBK").decode(bb);
+
+
+        cb=Charset.forName("GBK").decode(Charset.forName("GBK").encode("你好"));
+
+        for (int i = 0; i < cb.limit(); i++)  //打印“你好”
+            System.out.print(cb.get());
+    }
+}
+
+
+/****************************************************
+ * 作者：孙卫琴                                     *
+ * 来源：<<Java面向对象编程>>                       *
+ * 技术支持网址：www.javathinker.org                *
+ ***************************************************/
