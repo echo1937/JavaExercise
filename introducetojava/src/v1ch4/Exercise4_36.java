b35223d2009ad664e48a4885f5d6931a5741d1c5
Eric's Mac <numlock178@gmail.com>
2015-01-19 16:58:28 +0800

    8/26/2014 06:55

--- introducetojava/src/v1ch4/Exercise4_36.java
+++ introducetojava/src/v1ch4/Exercise4_36.java
@@ -7,37 +7,36 @@ import java.util.Scanner;
  * Created by Eric on 1/18/15.
  */
 public class Exercise4_36 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the first 9-digit of an ISBN number as integer: ");
-        int number=input.nextInt();
+        int number = input.nextInt();
 
 
-        int t=number;
-        int i=9;
-        int sum=0;
+        int t = number;
+        int i = 9;
+        int sum = 0;
 
-        while (t!=0){
-            sum+=(t%10)*i;
+        while (t != 0) {
+            sum += (t % 10) * i;
             i--;
-            t=t/10;
+            t = t / 10;
         }
 
-        int checksum=sum%11;
+        int checksum = sum % 11;
 
         System.out.print("This ISBN number is ");
 
-        int temp=100000000;
-        while (number/temp==0){
+        int temp = 100000000;
+        while (number / temp == 0) {
             System.out.print("0");
-            temp=temp/10;
+            temp = temp / 10;
         }
         System.out.print(number);
 
-        if(checksum==10){
+        if (checksum == 10) {
             System.out.print("X");
-        }
-        else{
+        } else {
             System.out.print(checksum);
         }
 
