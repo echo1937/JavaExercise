21ffa482985b933343f7c2b98f782ca08de57347
Eric <18657523064@139.com>
2015-05-28 09:33:31 +0800

    delete something

---
+++ introducetojava/src/v1ch1/Exercise1_2.java
@@ -0,0 +1,13 @@
+package v1ch1;
+
+/**
+ * Created by Eric on 5/28/2015.
+ */
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
