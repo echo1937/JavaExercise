593844ad6aa22e3424c5304e5f2e19b31b6a6e05
Eric <18657523064@139.com>
2015-06-24 22:09:59 +0800

    Helo

--- introducetojava/src/v1ch5/Exercise5_12.java
+++ introducetojava/src/v1ch5/Exercise5_12.java
@@ -9,7 +9,9 @@ public class Exercise5_12 {
 
     }
 
-    public static void printChars(char ch1, char ch2, int numberPerLine) {
+    public static void printChars(char ch1, char ch2, int numberPerLine)
+
+    {
         int count = 1;
         for (int i = ch1; i <= ch2; i++, count++) {
             if (count % numberPerLine == 0) {
