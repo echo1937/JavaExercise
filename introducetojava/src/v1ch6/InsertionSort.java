8b94527919c15ae9a2764f754156d30639367db1
Eric <18657523064@139.com>
2015-12-09 21:58:07 +0800

    insertionSort

--- introducetojava/src/v1ch6/InsertionSort.java
+++ introducetojava/src/v1ch6/InsertionSort.java
@@ -8,21 +8,36 @@ public class InsertionSort {
     public static void main(String[] args) {
         double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
         insertionSort(list);
-        for (int i = 0; i < list.length; i++) System.out.print(list[i] + "  ");
-
+        System.out.println();
+        r_insertionSort(list);
     }
 
     public static void insertionSort(double[] list) {
-        for (int i = 1; i < list.length; i++) {
+        for (int i = 1; i <= list.length - 1; i++) {
             double currentElement = list[i];
             int k;
 
             for (k = i - 1; k >= 0 && list[k] > currentElement; k--) list[k + 1] = list[k];
-
             list[k + 1] = currentElement;
 
-            for (int j = 0; j < list.length; j++) System.out.print(list[j] + "  ");
-            System.out.println();
+            printList(list);
         }
+    }
+
+    public static void r_insertionSort(double[] list) {
+        for (int i = 1; i <= list.length - 1; i++) {
+            double currentElement = list[i];
+            int k;
+
+            for (k = i - 1; k >= 0 && list[k] < currentElement; k--) list[k + 1] = list[k];
+            list[k + 1] = currentElement;
+            printList(list);
+        }
+    }
+
+
+    public static void printList(double[] list) {
+        for (int j = 0; j < list.length; j++) System.out.print(list[j] + "  ");
+        System.out.println();
     }
 }
