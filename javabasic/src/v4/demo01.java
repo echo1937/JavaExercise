73ef620ff005e2ea9b66e69e938ffc478f77778a
Eric <18657523064@139.com>
2016-01-24 23:04:24 +0800

    fix v4ch1, should start v5

--- javabasic/src/v4/demo01.java
+++ javabasic/src/v4/demo01.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 /**
  * Created by Eric on 1/24/2016.
  */
-public class demo01 {
+public class Demo01 {
     public static void main(String[] args) {
         int[] intArray = new int[101];
         Scanner input = new Scanner(System.in);
         int max = 0;
 
-        for (int count = 2; count < 0; ) {
+        for (int count = 2; count > 0; ) {
             int index = input.nextInt();
             intArray[index] += input.nextInt();
 
@@ -25,17 +25,40 @@ public class demo01 {
 
         }
 
-        while (max >= 0) {
+        boolean allZero = true;
+        for (int i = max; i > -1; i--) {
+            int coefficient = intArray[i];
 
-            if (intArray[max] != 0) {
+            if (coefficient != 0) {
+                allZero = false;
 
-                //把数组存储到k-v中,然后遍历map.
+                if (coefficient != 1 && coefficient != -1) {
+                    System.out.print(coefficient);
+                } else if (coefficient == -1) {
+                    System.out.print("-");
+                }
 
+                if (i != 0) {
+                    System.out.printf("x");
+                    if (i != 1) {
+                        System.out.printf("%d", i);
+                    }
+                }
+
+                for (int j = i - 1; j > -1; j--) {
+                    if (intArray[j] != 0) {
+                        if (intArray[j] > 0) {
+                            System.out.printf("+");
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
             }
-
-            max--;
         }
-
-
+        if (allZero) {
+            System.out.printf("0");
+        }
     }
 }
