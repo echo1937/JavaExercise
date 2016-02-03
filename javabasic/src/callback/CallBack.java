704bebcb0e91654faeccfad0081197aa2e219306
Eric <18657523064@139.com>
2016-02-03 21:25:51 +0800

    v5.1

---
+++ javabasic/src/callback/CallBack.java
@@ -0,0 +1,19 @@
+package callback;
+
+/**
+ * Created by Eric on 2/3/2016.
+ */
+
+/**
+ * 这是一个回调接口
+ *
+ * @author xiaanming
+ */
+public interface CallBack {
+    /**
+     * 这个是小李知道答案时要调用的函数告诉小王，也就是回调函数
+     *
+     * @param result 是答案
+     */
+    public void solve(String result);
+}
